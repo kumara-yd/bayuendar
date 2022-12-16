89ebbff1b2a04ee42322ed952e6db2332453a6f1
yayong <yayong.dk@excelindo.co.id>
2022-12-16 16:09:41 +0700

    commit server baru

--- js/custom2.js
+++ js/custom2.js
@@ -16,12 +16,15 @@ return xhr;
 
 
 function pesan() {
-    var url = 'https://bayuendar.000webhostapp.com/server.php?pesan=1';
+    var url = 'https://tools.te.net.id/cache/90192/server.php?pesan=1';
+    // var url = 'http://localhost/php8/wedding/server/server.php?pesan=1';
     var method = 'GET';
     var xhr = createCORSRequest(method, url);
 
-    xhr.onload = function (data) {
-        console.log(data);
+    xhr.onload = function () {
+        let data = JSON.parse(xhr.responseText);
+        // console.log(data);
+
         for (let i = 0; i < data.length; i++) {
             const val = JSON.parse(data[i]);
             const item = `
@@ -38,19 +41,20 @@ function pesan() {
     };
 
     xhr.onerror = function () {
-        alert('Gagal');
+        alert('Gagal mengambil pesan');
     };
 
     xhr.send();
 }
 
 function env() {
-    var url = 'https://bayuendar.000webhostapp.com/server.php?env=1';
+    var url = 'https://tools.te.net.id/cache/90192/server.php?env=1';
+    // var url = 'http://localhost/php8/wedding/server/server.php?env=1';
     var method = 'GET';
     var xhr = createCORSRequest(method, url);
 
-    xhr.onload = function (env) {
-        console.log(env);
+    xhr.onload = function () {
+        let env = JSON.parse(xhr.responseText);
         $('meta[name=description]').attr('content', env.title);
         $('meta[name=author]').attr('content', env.womenShort + " & " + env.menShort);
         $('title').append(env.title);
@@ -88,12 +92,71 @@ function env() {
     };
 
     xhr.onerror = function () {
-        alert('Gagal mengambil data.');
+        alert('Gagal mengambil env');
     };
 
     xhr.send();
 }
 
-pesan();
+function galery() {
+    var url = 'https://tools.te.net.id/cache/90192/server.php?gallery=1';
+    // var url = 'http://localhost/php8/wedding/server/server.php?gallery=1';
+    var method = 'GET';
+    var xhr = createCORSRequest(method, url);
+
+    xhr.onload = function () {
+        // let data = JSON.parse(xhr.responseText);
+        // console.log(xhr.responseText);
+        // Gallery
+        const lgContainer = document.getElementById('inline-gallery-container');
+        const inlineGallery = lightGallery(lgContainer, {
+            container: lgContainer,
+            dynamic: true,
+            thumbnail: true,
+            // Turn off hash plugin in case if you are using it
+            // as we don't want to change the url on slide change
+            hash: false,
+            // Do not allow users to close the gallery
+            closable: false,
+            // Add maximize icon to enlarge the gallery
+            showMaximizeIcon: true,
+            download: false,
+            // Append caption inside the slide item
+            // to apply some animation for the captions (Optional)
+            appendSubHtmlTo: '.lg-item',
+            // Delay slide transition to complete captions animations
+            // before navigating to different slides (Optional)
+            // You can find caption animation demo on the captions demo page
+            slideDelay: 400,
+            dynamicEl: xhr.responseText,
+        });
+
+        // Since we are using dynamic mode, we need to programmatically open lightGallery
+        inlineGallery.openGallery();
+    };
+
+    xhr.onerror = function () {
+        alert('Gagal mengambil gallery');
+    };
+
+    xhr.send();
+}
+
 
 env();
+pesan();
+galery();
+
+$(function(){
+    $("#player").click(function () {
+        if(this.className == 'Stop'){
+            $("audio").trigger("pause");
+            $(this).removeClass(this.className).addClass('Start');
+            $(this).html('Start');
+        } else {
+            $("audio").trigger("play");
+            $(this).removeClass(this.className).addClass('Stop');
+            $(this).html('Stop');
+        }
+    });
+})
